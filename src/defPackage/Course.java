5e3290ded5eff92047d04c274ca4e4bd84b3d162
xosrika <gkhos15@freeuni.edu.ge>
2017-06-08 23:07:19 +0400

    Crate course class
    add methods which returns all informathion from the course object;

--- src/defPackage/Course.java
+++ src/defPackage/Course.java
@@ -1,6 +1,7 @@
 package defPackage;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Course {
 	private ArrayList <Person> sectionLeaders;
@@ -10,14 +11,68 @@ public class Course {
 	private String courseName;
 	private String courseID;
 	
-	public Course (String courseName, String courseID, ArrayList <Person> sectionLeaders, ArrayList <Person> seminarists,
-			ArrayList <Person> students, ArrayList <Person> lecturers) {
+	//Constructor;
+	public Course (String courseName, String courseID, List <Person> sectionLeaders, List <Person> seminarists,
+			List <Person> students, List <Person> lecturers) {
 		this.courseName = courseName;
 		this.courseID = courseID;
 		this.sectionLeaders = new ArrayList<Person>(sectionLeaders);
 		this.seminarists = new ArrayList <Person> (seminarists);
 		this.students = new ArrayList <Person> (students);
 		this.lecturers = new ArrayList <Person> (lecturers);
+	}
+	
+	
+	/**
+	 * This method returns name of a course.
+	 * @return - name of a course;
+	 */
+	public String getCourseName(){
+		return this.courseName;
+		
+	}
+	
+	
+	/**
+	 * This method returns ID of a course.
+	 * @return - ID of a course 
+	 */
+	public String getCourseID(){
+		return this.courseID;
+	}
+	
+	
+	/**
+	 * This method returns section leaders' list from a course.
+	 * @return - section leaders list
+	 */
+	public List <Person> getCourseSectionLeaders(){
+		return this.sectionLeaders;
+	}
+	
+	
+	/**
+	 * this method returns seminarists' list from a course;
+	 * @return - list of a seminarists
+	 */
+	public List <Person> getCourseSeminarists(){
+		return this.seminarists;
+	}
+	
+	/**
+	 * This method returns students' list from a course;
+	 * @return - list of a students
+	 */
+	public List <Person> getCourseStudents(){
+		return this.students;		
+	}
+	
+	/**
+	 * This method returns lecturers' list from a course;
+	 * @return - list of a lecturers
+	 */
+	public List <Person> getCourseLecturers(){
+		return this.getCourseLecturers();
 	}
 	
 }
