87a5af49dbc7899a170734e5fa0840ea2418c484
cerushdope <aeonswander@gmail.com>
2017-06-08 22:07:28 +0400

    Added Context Listener
    Added context listener which initializes DBConnection class for context.

---
+++ src/Listeners/ContextListener.java
@@ -0,0 +1,38 @@
+package Listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import defPackage.DBConnection;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListener
+ *
+ */
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+        DBConnection connection = new DBConnection();
+        arg0.getServletContext().setAttribute("connection", connection);
+    }
+	
+}
