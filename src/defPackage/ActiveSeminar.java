e08f66ce507c4b0813ca61898d3afad4448b08a0
cerushdope <aeonswander@gmail.com>
2017-06-09 22:50:37 +0400

    Added classes
    Added classes which encapsulate Seminar, Active Seminar and Section.

---
+++ src/defPackage/ActiveSeminar.java
@@ -0,0 +1,54 @@
+package defPackage;
+
+public class ActiveSeminar {
+	
+	private String seminarId;
+	private String location;
+	private String name;
+	private String time;
+
+	public ActiveSeminar(String seminarId, String location, String name, String time) {
+		
+		this.seminarId = seminarId;
+		this.location = location;
+		this.name = name;
+		this.time = time;
+	}
+
+	/**
+	 * Returns seminar associated with this current active seminar.
+	 * 
+	 * @return
+	 */
+	public String getSeminarId() {
+		return this.seminarId;
+	}
+
+	/**
+	 * Returns location where this seminar is held.
+	 * 
+	 * @return
+	 */
+	public String getActiveSeminarLocation() {
+		return this.location;
+	}
+
+	/**
+	 * Returns name of the seminar.
+	 * 
+	 * @return
+	 */
+	public String getActiveSeminarName() {
+		return this.name;
+	}
+
+	/**
+	 * Returns time on which seminar is held.
+	 * 
+	 * @return
+	 */
+	public String getActiveSeminarTime() {
+		return this.time;
+	}
+
+}
