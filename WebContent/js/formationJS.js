db412327e67e8965c9e62bd38e9bb204190c3ec5
cxovrika <cxovrebovi@gmail.com>
2017-06-10 07:41:30 -0700

    Added block that displays seminars and active seminars
    These two blocks display abstract seminars and active seminars with their locations and start times.

--- WebContent/js/formationJS.js
+++ WebContent/js/formationJS.js
@@ -1,9 +1,16 @@
 function main() {
   $(".group-persons").hide();
+  $(".grevent-items").hide();
   
   $(".display-persons-button").on("click",
 	function (){
     $(this).next().slideToggle(400);
+    //$(this).toggleClass('active');
+  });
+  
+  $(".display-grevent-button").on("click",
+	function (){
+	$(this).next().slideToggle(400);
     //$(this).toggleClass('active');
   });
   
