2cf4c4170533fc6559cf7f3fe300e93cf7f89e1b
xosrika <gkhos15@freeuni.edu.ge>
2017-06-20 13:55:44 +0400

    PostServlet now sends to mails to students;
    After post is added to the main page, the text of the post is sent to all STUDENTS from classroom; Changed mailConnector constuctor so it takes list of emails, where we must send emails;

--- src/WorkingServlets/PostServlet.java
+++ src/WorkingServlets/PostServlet.java
@@ -1,6 +1,9 @@
 package WorkingServlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +14,8 @@ import Dummys.PersonGeneratorDummy;
 import Listeners.ContextListener;
 import database.AllConnections;
 import defPackage.Classroom;
+import defPackage.MailConnector;
+import defPackage.Person;
 
 /**
  * Servlet implementation class PostServlet
@@ -43,8 +48,17 @@ public class PostServlet extends HttpServlet {
 		String postText = request.getParameter("postText");
 		String personId = PersonGeneratorDummy.createPersonByEmail("random@random.com").getPersonID();
 		
-		AllConnections connection = (AllConnections)request.getServletContext().getAttribute(ContextListener.CONNECTION_ATTRIBUTE_NAME);
-		
+		AllConnections connection = (AllConnections)request.getServletContext()				
+				.getAttribute(ContextListener.CONNECTION_ATTRIBUTE_NAME);
+		Classroom currentClassroom = connection.classroomDB.getClassroom(classroomId);
+		List<Person> allStudents = currentClassroom.getClassroomStudents();
+		ArrayList<String> emails  = new ArrayList <String>(); 
+		for(Person person : allStudents){
+			emails.add(person.getEmail());
+		}
+		MailConnector mail = new MailConnector(emails, "subject", postText);
+		mail.sendMail();
+			
 		connection.postDB.addPost(classroomId, personId, postText);
 		response.sendRedirect("stream.jsp?" + Classroom.ID_ATTRIBUTE_NAME + "=" + classroomId);
 	}
